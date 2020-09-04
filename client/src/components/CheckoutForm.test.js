b0fe9b2d628a937d0708d5befdab731afd9d5cac
William Herman <williamajherman@gmail.com>
2020-09-04 12:03:42 -0400

    first test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
     // Arrange
+    render(<CheckoutForm/>)
     // Act
+    const reactHeader = screen.findByText(/react plants/i);
+    const checkoutHeader = screen.findByText(/checkout from/i);
     // Assert
 });
 
 test("form shows success message on submit with form details", () => {
+    // Arrange
+    // Act
+    // Assert
 
 });
