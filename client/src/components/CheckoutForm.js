9d3fbf417628a2e4bd585f6fca5154a849c39045
William Herman <williamajherman@gmail.com>
2020-09-04 11:51:34 -0400

    added form hook

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import usePlantForm from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -15,10 +16,10 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, setValues] = usePlantForm(initialValue);
 
   const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
+    setValues(e);
   };
 
   const handleSubmit = (e) => {
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+const usePlantForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues);
+
+    return [
+        values,
+        function (event){
+            setValues({
+                ...values,
+                [event.target.name]: event.target.value
+            })
+        }
+    ]
+}
+
+export default usePlantForm;
