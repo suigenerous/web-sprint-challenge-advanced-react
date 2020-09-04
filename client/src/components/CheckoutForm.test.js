81b33d28a04323e8cb4db91cc772c5162fc64936
William Herman <williamajherman@gmail.com>
2020-09-04 11:58:43 -0400

    fixed hook

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,12 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    // Arrange
+    // Act
+    // Assert
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+
+});
